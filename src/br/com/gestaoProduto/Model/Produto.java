d78ea0773d365d842db275b6c7ccb0fe462ccf9f
shelldonryan <shelldon.boy15@gmail.com>
2023-09-12 20:22:07 -0300

    Change Class Produto

--- src/br/com/gestaoProduto/Model/Produto.java
+++ src/br/com/gestaoProduto/Model/Produto.java
@@ -1,5 +1,28 @@
 package br.com.gestaoProduto.Model;
 
+import java.util.UUID;
+
 public class Produto {
-    
+    public UUID idDoProduto;
+    public String nome;
+    public String desc;
+    public Double valor;
+    public int quantidadeEmEstoque;
+
+    public Produto(String nomeDoProduto, String descDoProduto, Double valorDoProduto, int qntEstoque) {
+        this.nome = nomeDoProduto;
+        this.desc = descDoProduto;
+        this.valor = valorDoProduto;
+        this.quantidadeEmEstoque = qntEstoque;
+    }
+
+    public void exibirInfo() {
+        System.out.printf("""
+                \nDetalhes do Produto:
+                Nome: %s
+                Descricao: %s
+                Valor: R$ %.2f
+                Quantidade no estoque: %d
+                """, nome, desc, valor, quantidadeEmEstoque);
+    }
 }
